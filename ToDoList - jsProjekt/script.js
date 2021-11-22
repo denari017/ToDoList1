9c69cb854348627da6d2e243a340b4b5e3791097
denari017 <84735521+denari017@users.noreply.github.com>
2021-11-22 18:51:22 +0100

    Add files via upload

---
+++ ToDoList - jsProjekt/script.js
@@ -0,0 +1,16 @@
+var inputBarId = document.getElementById("inputBarId");
+var addButtonId = document.getElementById("ButtonId");
+var toDos = document.getElementById("toDos");
+
+addButtonId.addEventListener("click", function(){
+    var x = document.createElement("p");
+    x.innerText = inputBarId.value;
+    toDos.appendChild(x);
+
+x.addEventListener("dblclick", function(){
+    toDos.removeChild(x);
+
+});
+});
+
+exports.build = series(scssTask, jsTask);
