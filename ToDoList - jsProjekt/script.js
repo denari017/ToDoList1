d5091359eab86d3ca0cf6a673fe9553806f19d69
denari017 <84735521+denari017@users.noreply.github.com>
2021-11-22 20:18:57 +0100

    Update script.js

--- ToDoList - jsProjekt/script.js
+++ ToDoList - jsProjekt/script.js
@@ -2,11 +2,16 @@ var inputBarId = document.getElementById("inputBarId");
 var addButtonId = document.getElementById("ButtonId");
 var toDos = document.getElementById("toDos");
 
-addButtonId.addEventListener("click", function(){
+addButtonId.addEventListener("click", function(){  //moznos pridat todo 
     var x = document.createElement("p");
     x.innerText = inputBarId.value;
     toDos.appendChild(x);
-x.addEventListener("dblclick", function(){
+    
+    x.addEventListener("click", function(){ // vičiarknúť čo už je hotové
+        x.style.textDecoration = "line-through";
+    });
+    
+x.addEventListener("dblclick", function(){ // remove todo 
     toDos.removeChild(x);
 
 });
